44314fe616f221560fcc02841aac272ba32df3cb
hyeongjun <whow1101@naver.com>
2024-07-04 01:49:47 +0900

    feat: firebase 모듈 추가 - firebase class로 변환 - omctDB 이미지 서버 전송 및 응답 기능 추가 - util 함수 추가 (dataExtension)

--- src/utils/omctDb.ts
+++ src/utils/omctDb.ts
@@ -1,18 +1,35 @@
-import { doc, getDoc, setDoc } from 'firebase/firestore';
-import { db } from './firebase';
+import {
+  DocumentData,
+  DocumentReference,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
-class OmctDb {
+import OmctFirebaseApp from '@Utils/firebase';
+import { getRandomId } from '@Base/utils/dataExtension';
+import { parseError } from '@Utils/omctError';
+import omctError, { EOmctErrorNo } from '@Constant/errorKeyValue';
+
+class OmctDb extends OmctFirebaseApp {
   static #instance: InstanceType<typeof OmctDb>;
+  static readonly MAX_RETRY_CNT = 3;
 
-  #docRef = doc(db, 'numberOfUsers', 'numberOfUsers');
+  private m_docRef: DocumentReference<DocumentData>;
+  private m_personaImagePath: string;
 
   constructor() {
+    super();
     if (OmctDb.#instance) return OmctDb.#instance;
     OmctDb.#instance = this;
+
+    this.m_docRef = doc(this.m_db, 'numberOfUsers', 'numberOfUsers');
+    this.m_personaImagePath = 'personal-images';
   }
 
-  async getNumberOfUsers() {
-    const docSnap = await getDoc(this.#docRef);
+  public async getNumberOfUsers() {
+    const docSnap = await getDoc(this.m_docRef);
 
     try {
       if (docSnap.exists()) {
@@ -30,9 +47,49 @@ class OmctDb {
     }
   }
 
-  async addNumberOfUsers() {
+  public async addNumberOfUsers() {
     const addedNumberOfUsers = (await this.getNumberOfUsers()) + 1;
-    setDoc(this.#docRef, { numberOfUsers: addedNumberOfUsers });
+    setDoc(this.m_docRef, { numberOfUsers: addedNumberOfUsers });
+  }
+
+  public async sendPersonalImage(file: Blob | Uint8Array | ArrayBuffer) {
+    let uniqueId = getRandomId();
+    let cnt = 0;
+    for (cnt = 0; cnt < OmctDb.MAX_RETRY_CNT; cnt++) {
+      console.log(uniqueId);
+      try {
+        await this.getPersonalImageUrl(uniqueId);
+      } catch (e: unknown) {
+        const err = parseError(e);
+        if (err.code === EOmctErrorNo.FIREBASE_STORAGE_OBJECT_NOT_FOUND) {
+          break;
+        }
+      }
+      uniqueId = getRandomId();
+    }
+
+    const isFail = cnt === OmctDb.MAX_RETRY_CNT;
+
+    if (isFail)
+      throw {
+        code: EOmctErrorNo.FIREBASE_STORAGE_UNIQUE_ID_NOT_FOUND,
+        message: omctError.get(
+          EOmctErrorNo.FIREBASE_STORAGE_UNIQUE_ID_NOT_FOUND
+        ),
+      } as OmctErrorResponse;
+
+    const imgRef = ref(
+      this.m_storage,
+      `${this.m_personaImagePath}/${uniqueId}.png`
+    );
+
+    return uploadBytes(imgRef, file);
+  }
+
+  public async getPersonalImageUrl(name: string) {
+    const path = `${this.m_personaImagePath}/${name}.png`;
+    const imgRef = ref(this.m_storage, path);
+    return getDownloadURL(imgRef);
   }
 }
 
