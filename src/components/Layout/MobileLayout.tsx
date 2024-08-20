97531846ee84a5cb14ef7390d3990899eb884112
seoltang <yong_drg@naver.com>
2024-08-21 04:33:12 +0900

    refactor: 이전 다국어 라이브러리 react-intl 주석처리 부분 및 사용하지 않는 다국어 recoil atom 삭제

--- src/components/Layout/MobileLayout.tsx
+++ src/components/Layout/MobileLayout.tsx
@@ -2,16 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import useViewportHeight from '@Hooks/useViewportHeight';
 import { useRecoilValue } from 'recoil';
-// import { useRecoilValue, useRecoilState } from 'recoil';
 import {
   globalBgColorAtom,
   globalTextColorAtom,
 } from '@Recoil/globalStyleStore';
-// import { IntlProvider } from 'react-intl';
-// import { Locale } from '@Recoil/app';
-// import flattenMessages from '@Utils/flattenMessages';
-// import koLanguage from './../../../public/translations/ko.json';
-// import EnLanguage from './../../../public/translations/en.json';
 
 interface MobileLayoutProps {
   backgroundColor: string;
@@ -30,22 +24,11 @@ const Wrapper = styled.div<MobileLayoutProps>`
 function MobileLayout({ children }: React.PropsWithChildren) {
   const bgColor = useRecoilValue(globalBgColorAtom);
   const textColor = useRecoilValue(globalTextColorAtom);
-  // const [locale] = useRecoilState(Locale);
   useViewportHeight();
-
-  // const translationsForUsersLocale = {
-  //   'en-US': EnLanguage,
-  //   'ko-KR': koLanguage,
-  // }[locale];
 
   return (
     <Wrapper backgroundColor={bgColor} textColor={textColor}>
-      {/* <IntlProvider
-        locale={locale}
-        messages={flattenMessages(translationsForUsersLocale)}
-      > */}
       {children}
-      {/* </IntlProvider> */}
     </Wrapper>
   );
 }
